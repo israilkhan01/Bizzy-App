b2b7c96ef72c2712f96c1c1510a3812a21f89f51
israilkhan01 <44443989+israilkhan01@users.noreply.github.com>
2020-03-26 16:37:57 +0530

    creating another router and controller

---
+++ controllers/user_controller.js
@@ -0,0 +1,4 @@
+module.exports.profile=function(req,res){
+
+    return res.end("<h1> Hey this is Profile Page!</h1>")
+}
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ const homeController=require("../controllers/home_controller")
 console.log("Router loaded");
 
 router.get('/',homeController.home);
+router.use('/users',require("./user"))
+
+//for any further routes access from here
+//router.use('/routername',require("./routerfile"))
 module.exports=router;
