3dd13825b7b993978862006435b9027a3fa033b7
israilkhan01 <44443989+israilkhan01@users.noreply.github.com>
2020-03-28 02:09:52 +0530

    styling

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const expressLayouts=require('express-ejs-layouts');
 //use express router
 app.use(express.static('./assets'))
 app.use(expressLayouts);
+//extracts layouts and scripts from subpages into the layout
+app.set('layout extractStyles',true)
+app.set('layout extractScripts',true)
+
 app.use('/',require('./routes'));
 //setting up view Engine
 app.set('view engine','ejs');
