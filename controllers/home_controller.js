b7ed3a735b71e943f5318bc43b511f66f2f25837
israilkhan01 <44443989+israilkhan01@users.noreply.github.com>
2020-04-06 23:36:40 +0530

    Display Posts and related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,19 @@
+const Post=require('../models/post');
 module.exports.home=function(req,res){
 
     // return res.end('<h1>Expresss is up for Bizzy App!</h1>');
     console.log(req.cookies);
-    return res.render('home',{
-        title:"Home"
+    // Post.find({},function(err,post){
+   
+    //     return res.render('home',{
+    //         title:"Bizzy | Home",
+    //         posts:post
+    //     });
+    // });
+    Post.find({}).populate('user').exec(function(err,post){
+        return res.render('home',{
+            title:"Bizzy | Home",
+            posts:post
+        });
     })
 }
