d3a62c23e10bf5a52a5f83c997aa8c344a429b72
israilkhan01 <44443989+israilkhan01@users.noreply.github.com>
2020-04-09 00:36:35 +0530

    deleted post (Authourised)

--- routes/posts.js
+++ routes/posts.js
@@ -6,5 +6,5 @@ const router=express.Router();
 const postController=require('../controllers/post_controller');
 
 router.post('/create',passpot.checkAuthentication,postController.create);
-
+router.get('/destroy/:id',passpot.checkAuthentication,postController.destroy);
 module.exports=router;
