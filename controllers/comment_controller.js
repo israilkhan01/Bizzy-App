047211414860282e7d28c8dfc28254e818978689
israilkhan01 <44443989+israilkhan01@users.noreply.github.com>
2020-04-10 02:30:29 +0530

    Deleting a Comment (Authorized)

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,9 +1,9 @@
-const comment=require('../models/comment');
+const Comment=require('../models/comment');
 const Post=require('../models/post')
 module.exports.create=function(req,res){
     Post.findById(req.body.post,function(err,post){
         if(post){
-            comment.create({
+            Comment.create({
                 content:req.body.content,
                 post:req.body.post,
                 user:req.user._id
@@ -15,4 +15,20 @@ module.exports.create=function(req,res){
             })
         }
     });
+}
+module.exports.destroy=function(req,res){
+    Comment.findById(req.params.id,function(err,comment){
+           if(comment.user==req.user.id || comment.post){
+               let postId=comment.post;
+               comment.remove();
+               Post.findByIdAndUpdate(postId,{ $pull:{comments:req.params.id} },function(err,post){
+                   console.log('error')
+                   return res.redirect('back')
+               });
+           }
+           else{
+           
+               return res.redirect('back');
+           }
+    })
 }
--- routes/comments.js
+++ routes/comments.js
@@ -6,5 +6,6 @@ const router=express.Router();
 const commentController=require('../controllers/comment_controller');
 
 router.post('/create',passpot.checkAuthentication,commentController.create);
+router.get('/destroy/:id',passpot.checkAuthentication,commentController.destroy);
 
 module.exports=router;
