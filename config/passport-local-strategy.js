84bc1b011bc12a0fcee0ab770f81bfeb324b8bc0
israilkhan01 <44443989+israilkhan01@users.noreply.github.com>
2020-04-12 01:57:18 +0530

    Adding Flash Messages to more Actions

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -4,16 +4,18 @@ const LocalStrategy=require('passport-local').Strategy;
 const User=require('../models/user');
 //authentication using passport
 passport.use(new LocalStrategy({
-        usernameField:'email'
+        usernameField:'email',
+        passReqToCallback:true
         },
-    function(email,password,done){
+    function(req,email,password,done){
    //find a user esteblishing the identity
    User.findOne({email:email},function(err,user){
-       if(err){ console.log('error in finding user--> passport')
+       if(err){
+           req.flash('error',err)
             return done(err);
         }
       if(!user||user.password!=password){
-          console.log('password invalid');
+          req.flash('error','Password invalid')
           return done(null,false);
       }
       return done(null,user);
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -7,8 +7,10 @@ module.exports.create= async function(req,res){
             content:req.body.content,
             user:req.user._id,
         });
+        req.flash('success','Post Published')
         return res.redirect('back');
     }catch(err){
+        req.flash('error',err)
         console.log('error',err);
     }
 }
@@ -18,12 +20,13 @@ module.exports.destroy=async function(req,res){
         if(post.user==req.user.id){
             post.remove();
             await Comment.deleteMany({post:req.params.id});
+            req.flash('success','Post and associated Comments Deleted')
             return res.redirect('back');
          }else{
            return res.redirect('back');
          }
     }catch(err){
-            console.log('Error',err);
-            return ;
+        req.flash('error','You can not delete this post')
+            return res.redirect('back');
     }
 }
