c45cf188b50813a80e9fe4bc811a2180c1a5e9b8
israilkhan01 <44443989+israilkhan01@users.noreply.github.com>
2020-04-11 17:33:34 +0530

    Converting to Async Await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,32 +1,27 @@
 const Post=require('../models/post');
 const User=require('../models/user');
-module.exports.home=function(req,res){
+module.exports.home=async function(req,res){
 
-    // return res.end('<h1>Expresss is up for Bizzy App!</h1>');
-    console.log(req.cookies);
-    // Post.find({},function(err,post){
-   
-    //     return res.render('home',{
-    //         title:"Bizzy | Home",
-    //         posts:post
-    //     });
-    // });
-    Post.find({})
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate:{
-            path:'user'
-        }
-    })
-    .exec(function(err,post){
-        User.find({},function(err,user){
-            return res.render('home',{
-                title:"Bizzy | Home",
-                posts:post,
-                all_users:user
-            });
-        })
-        
-    })
+  
+   try{
+        console.log(req.cookies);
+    
+        let posts=await Post.find({})
+        .populate('user')
+        .populate({
+            path:'comments',
+            populate:{
+                path:'user'
+            }
+        });
+        let users=await User.find({});
+
+        return res.render('home',{
+            title:"Bizzy | Home",
+            posts:posts,
+            all_users:users
+        });
+   }catch(err){
+            console.log('Error',err);
+   }
 }
