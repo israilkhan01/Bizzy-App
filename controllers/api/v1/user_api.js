61ac68c21d031257134066d68761f8e48f6212d8
israilkhan01 <44443989+israilkhan01@users.noreply.github.com>
2020-04-23 21:21:34 +0530

    Created a token using Jwt

---
+++ controllers/api/v1/user_api.js
@@ -0,0 +1,27 @@
+const User=require('../../../models/user');
+const jwt=require('jsonwebtoken');
+
+module.exports.createSession=async function(req,res){
+    try{
+        let user=await User.findOne({email:req.body.email});
+
+        if(!user||user.password!=req.body.password){
+            return res.json(422,{
+              message:'invalid username or password'
+            });
+        }
+        return res.json(200,{
+            message:'sign in succesfully here is your token please keep it safe',
+            data:{
+                token:jwt.sign(user.toJSON(),'bizzy',{expiresIn:1000})
+            }
+        })
+    }catch(err){
+        console.log("***",err);
+        return res.status(500).json({
+            message:'internal server error',
+        });
+
+    }
+
+}
