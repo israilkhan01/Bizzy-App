6deb09ca279b95612857f420e572a4f8f6881121
israilkhan01 <44443989+israilkhan01@users.noreply.github.com>
2020-05-12 18:42:16 +0530

    styled sign in page and handled Authentication over home page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const express=require("express");
-
+const passport=require('passport');
 const router=express.Router();
 const homeController=require("../controllers/home_controller")
 console.log("Router loaded");
 
-router.get('/',homeController.home);
+router.get('/',passport.checkAuthentication,homeController.home);
 router.use('/users',require('./user'));
 router.use('/posts',require('./posts'));
 router.use('/comments',require('./comments'));
