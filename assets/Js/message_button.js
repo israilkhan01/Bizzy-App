f7fa60e90f3cd554213b010e1b8443fb7463a2d4
israilkhan01 <44443989+israilkhan01@users.noreply.github.com>
2020-05-22 23:56:09 +0530

    styling up home page

--- assets/Js/message_button.js
+++ assets/Js/message_button.js
@@ -1,13 +1,19 @@
 var msg=document.getElementById('message-chatbox');
 msg.addEventListener('click',function(event){
     event.preventDefault();
+    // var chatbox=document.getElementsByClassName('chatting');
+    // console.log(chatbox);
+    // chatbox[0].style.visibility="visible";
     var chatbox=document.getElementsByClassName('chatting');
-    console.log(chatbox);
-    chatbox[0].style.visibility="visible";
+    if (chatbox[0].style.display === "none") {
+        chatbox[0].style.display = "block";
+    } else {
+        chatbox[0].style.display = "none";
+    }
 });
 
 var removebox=$('.remove-message-box');
 removebox.click(function(event){
     console.log($('.chatting'));
-    $('.chatting')[0].style.visibility="hidden";
+    $('.chatting')[0].style.display = "none";
 })
