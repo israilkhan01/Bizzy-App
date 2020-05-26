dda22033f05646576be0a6b00c167c375b3780ab
israilkhan01 <44443989+israilkhan01@users.noreply.github.com>
2020-05-27 01:02:53 +0530

    click event outside open div

--- assets/Js/user_profile.js
+++ assets/Js/user_profile.js
@@ -1,12 +1,12 @@
 
-var handler=false;
+let handler=false;
 let editButton=$('.edit-profile-button');
 editButton.click(function(){
     $('.profile-page').css('opacity','0.6');
     $('.edit-pops').css('display','block');
-    // $('.edit-pops').css("slow");
     $('.edit-pops').css('opacity','1');
     handler=true;
+    // console.log(handler);
 });
 let removebutton=$('.remv');
 console.log(removebutton);
@@ -14,11 +14,13 @@ removebutton.click(function(){
     $('.profile-page').css('opacity','1');
     $('.edit-pops').css('display','none');
     handler=false;
+    // console.log(handler);
 });
-if(handler){
-    $('body').click(function(){
+// console.log(handler);
+$(document).mouseup(function (event) { 
+    console.log($(event.target).closest(".edit-pops").length)
+    if ($(event.target).closest(".edit-pops").length=== 0) { 
+        $(".edit-pops").hide(); 
         $('.profile-page').css('opacity','1');
-        $('.edit-pops').css('display','none');
-    });
-}
-
+    } 
+}); 
