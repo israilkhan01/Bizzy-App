1b570b16611d73eab7edb3e6bf9b51d6a8c8adc2
israilkhan01 <44443989+israilkhan01@users.noreply.github.com>
2020-05-27 00:24:07 +0530

    handled remove button in edit-profile form

--- assets/Js/user_profile.js
+++ assets/Js/user_profile.js
@@ -1,9 +1,24 @@
 
+var handler=false;
 let editButton=$('.edit-profile-button');
 editButton.click(function(){
     $('.profile-page').css('opacity','0.6');
     $('.edit-pops').css('display','block');
     // $('.edit-pops').css("slow");
     $('.edit-pops').css('opacity','1');
+    handler=true;
 });
+let removebutton=$('.remv');
+console.log(removebutton);
+removebutton.click(function(){
+    $('.profile-page').css('opacity','1');
+    $('.edit-pops').css('display','none');
+    handler=false;
+});
+if(handler){
+    $('body').click(function(){
+        $('.profile-page').css('opacity','1');
+        $('.edit-pops').css('display','none');
+    });
+}
 
