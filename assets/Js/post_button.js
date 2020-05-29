64b8a1d7e21926d3415b6f7c2a9be2be80ff68a5
israilkhan01 <44443989+israilkhan01@users.noreply.github.com>
2020-05-29 15:50:10 +0530

    styled up profile page

--- assets/Js/post_button.js
+++ assets/Js/post_button.js
@@ -1,24 +1,16 @@
-// let Button=document.getElementsByClassName('post-option-button');
-// console.log(Button,"button");
-// for(let but of Button){
-//     but.addEventListener("click",function(){
-//         let del=document.getElementsByClassName(`post-delete-${post.id}`);
-//         console.log(del);
-//     })
-// }
 class postOptions{
-      constructor(postId){
+    constructor(postId){
         this.postId = postId;
         this.clicking(postId);
       }
-      clicking(postId){
-          let option =document.getElementById(`post-option-${postId}`);
-          console.log(option);
-          option.addEventListener('click',function(){
-            let del=document.getElementById(`post-delete-${postId}`);
-            console.log("del",del,postId);
-            del.style.display='block';
-            
-          })
+    clicking(postId){
+        let option =document.getElementById(`post-option-${postId}`);
+        console.log(option);
+        option.addEventListener('click',function(){
+              let del=document.getElementById(`post-delete-${postId}`);
+              console.log("del",del,postId);
+              del.style.display='block';    
+        });
+        
       }
 }
